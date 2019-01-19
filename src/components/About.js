c01bbcaeb72a6ce5b493b074d15fab147be9380b
Lawrence <Lawrence4code@gmail.com>
2019-01-19 23:59:01 +0530

    About section

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/header.scss';
+
+const About = () => {
+  const iconColor = '#dcf2f0';
+
+  return (
+    <div class="about">
+      <div class="">
+        <h2>That's me...</h2>
+
+        <p>
+          Confident and dedicated photographer with experience in both
+          professional and freelance photography. Holds nearly 10 years of
+          working experience with great variety in order to tackle any
+          photography job quickly and effectively. Intimately familiar taking
+          high quality digital photographs, including framing, selecting and
+          setting up lighting, and determining advanced shutter and lens
+          options.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
